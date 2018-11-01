258cc691deda44949f7eed1f183653ef672aec24
murchant <mamurtag@tcd.ie>
2018-11-01 11:28:34 +0000

    Parser works, date field needs work

--- project/src/main/java/com/wbs/app/App.java
+++ project/src/main/java/com/wbs/app/App.java
@@ -1,4 +1,34 @@
 package com.wbs.app;
+import java.util.ArrayList;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Date;
+import java.util.ArrayList;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  * Hello world!
@@ -8,6 +38,16 @@ public class App
 {
     public static void main( String[] args )
     {
+    	FrParser myParser = new FrParser();
+    	ArrayList<Document> myDocs = myParser.parseFile();
+    	for(int i=0; i< myDocs.size(); i++){
+    		System.out.println(myDocs.get(i).get("id"));
+    		System.out.println(myDocs.get(i).get("filename"));
+    		System.out.println(myDocs.get(i).get("date"));
+    		System.out.println(myDocs.get(i).get("text"));
+    		System.out.println("///////////////////////////////////////////////////////////////////////////");
+
+    	}
         System.out.println( "Hello World!" );
     }
 }
