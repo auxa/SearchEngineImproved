0c68154cecb92b837222f54123cb5b43571937fd
Yash Mundra <yash@Yashs-MacBook-Pro.local>
2018-11-05 15:52:00 +0000

    Added Fbis parser

---
+++ project/src/main/java/com/wbs/app/FbisParser.java
@@ -0,0 +1,58 @@
+package com.wbs.app;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class FbisParser{
+
+    public FbisParser(){}
+
+    public String FBIS_FOLDER = "../project/AssignmentTwo/fbis";
+
+    public ArrayList<Document> parseFile(){
+        ArrayList<Document> docList = new ArrayList<Document>();
+        File dir = new File(FBIS_FOLDER);
+        File[] docs = dir.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                if(name.contains("readchg") || name.contains("readmefb")) {
+                    return false;
+                }
+                return  true;
+            }
+        });
+        for(File inputDoc: docs) {
+            try {
+                org.jsoup.nodes.Document doc = Jsoup.parse(inputDoc, "UTF-8");
+                Elements documents = doc.getElementsByTag("DOC");
+                for (Element docI : documents) {
+                    String docNum = docI.getElementsByTag("DOCNO").text();
+                    String textField = docI.getElementsByTag("TEXT").text();
+                    String dateElement = docI.getElementsByTag("DATE1").text();
+                    // Capturing every F tag for now even the ones in Text field
+                    String title = docI.getElementsByTag("HEADER").text() + " " + docI.getElementsByTag("ABS").text() + " " + docI.getElementsByTag("F").text();
+
+                    Document customDoc = new Document();
+                    customDoc.add(new TextField("id", docNum, Field.Store.YES));
+                    customDoc.add(new TextField("filename", title, Field.Store.YES));
+                    customDoc.add(new TextField("text", textField, Field.Store.YES));
+                    customDoc.add(new TextField("date", dateElement, Field.Store.YES));
+                    docList.add(customDoc);
+                    System.out.println("added doc number: " + docNum);
+                }
+                System.out.println(docList.size());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return docList;
+    }
+}
