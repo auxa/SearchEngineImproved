5a79da938b475e53de38c4d5191f8b77cd0bc81d
Yash Mundra <yash@Yashs-MacBook-Pro.local>
2018-11-24 16:58:35 +0000

    updates

--- project/src/main/java/com/wbs/app/CustomAnalyzer.java
+++ project/src/main/java/com/wbs/app/CustomAnalyzer.java
@@ -1,9 +1,7 @@
 package com.wbs.app;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.text.ParseException;
 import java.util.ArrayList;
 
 import org.apache.lucene.analysis.CharArraySet;
@@ -12,7 +10,11 @@ import org.apache.lucene.analysis.StopFilter;
 import org.apache.lucene.analysis.StopwordAnalyzerBase;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.core.FlattenGraphFilter;
 import org.apache.lucene.analysis.en.PorterStemFilter;
+import org.apache.lucene.analysis.miscellaneous.TrimFilter;
+import org.apache.lucene.analysis.snowball.SnowballFilter;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.analysis.core.StopAnalyzer;
 import org.apache.lucene.analysis.en.EnglishMinimalStemFilter;
@@ -20,23 +22,33 @@ import org.apache.lucene.analysis.en.EnglishPossessiveFilter;
 import org.apache.lucene.analysis.en.PorterStemFilter;
 import org.apache.lucene.analysis.standard.StandardFilter;
 import org.apache.lucene.analysis.standard.ClassicTokenizer;
+import org.apache.lucene.analysis.synonym.SynonymGraphFilter;
+import org.apache.lucene.analysis.synonym.SynonymMap;
+import org.apache.lucene.analysis.synonym.WordnetSynonymParser;
+import org.tartarus.snowball.ext.EnglishStemmer;
 
 
 public class CustomAnalyzer extends StopwordAnalyzerBase{
 
 	protected TokenStreamComponents createComponents(String s) {
-		final Tokenizer source = new StandardTokenizer();
 
-		TokenStream tok = new LowerCaseFilter(source);
-		try {
-			tok = new StopFilter(tok, getStopWords());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		tok = new PorterStemFilter(tok);
-		return new TokenStreamComponents(source, tok);
+        final Tokenizer source = new StandardTokenizer();
+        TokenStream tok = new LowerCaseFilter(source);
 
-	}
+        try {
+            tok = new TrimFilter(tok);
+//            tok = new SynonymGraphFilter(tok, buildMap(), true);
+//            tok = new FlattenGraphFilter(tok);
+            tok = new SynonymAwareStopFilter(tok, getStopWords());
+            tok = new SnowballFilter(tok, new EnglishStemmer());
+            //        tok = new PorterStemFilter(tok);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new TokenStreamComponents(source, tok);
+
+
+    }
 
 	protected CharArraySet getStopWords() throws IOException {
 		FileInputStream fileInputStream = new FileInputStream("../project/stopwords.txt");
@@ -51,4 +63,12 @@ public class CustomAnalyzer extends StopwordAnalyzerBase{
 		bufferedReader.close();
 		return  new CharArraySet(stopWordsSet, true);
 	}
+
+	protected SynonymMap buildMap() throws IOException, ParseException {
+        FileReader reader = new FileReader("../project/wn_s.pl");
+        WordnetSynonymParser synonymParser = new WordnetSynonymParser(true, true, new StandardAnalyzer(CharArraySet.EMPTY_SET));
+        synonymParser.parse(reader);
+        SynonymMap synonymMap = synonymParser.build();
+        return synonymMap;
+    }
 }
